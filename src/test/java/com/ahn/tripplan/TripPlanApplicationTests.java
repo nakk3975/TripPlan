695370ef0c734cffb463de00d8b5e541f15a8433
AHN <dsfs3975@naver.com>
2023-03-03 19:45:42 +0900

    [안재원]  - 최초 파일 추가

---
+++ src/test/java/com/ahn/tripplan/TripPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahn.tripplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
